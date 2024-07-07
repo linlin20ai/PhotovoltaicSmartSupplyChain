31a8896dec1a79e244663b10462f6c835861a138
linlin20ai <168981122+linlin20ai@users.noreply.github.com>
2024-07-07 11:15:58 +0800

    Add files via upload

---
+++ PhotovoltaicSmartSupplyChain/src/main/java/com/putianhouduan/PhotovoltaicSmartSupplyChain/PhotovoltaicSmartSupplyChainApplication.java
@@ -0,0 +1,13 @@
+package com.putianhouduan.PhotovoltaicSmartSupplyChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotovoltaicSmartSupplyChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotovoltaicSmartSupplyChainApplication.class, args);
+    }
+
+}
---
+++ PhotovoltaicSmartSupplyChain/src/test/java/com/putianhouduan/PhotovoltaicSmartSupplyChain/PhotovoltaicSmartSupplyChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putianhouduan.PhotovoltaicSmartSupplyChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotovoltaicSmartSupplyChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

71a2e6c421231ce7a94a198796c6fe6b5a744f52
linlin20ai <3357929671@qq.com>
2024-07-20 13:17:32 +0800

    OrderController的list方法修改和一些小瑕疵修改

--- PhotovoltaicSmartSupplyChain/src/main/java/com/putianhouduan/PhotovoltaicSmartSupplyChain/controller/OrderController.java
+++ PhotovoltaicSmartSupplyChain/src/main/java/com/putianhouduan/PhotovoltaicSmartSupplyChain/controller/OrderController.java
@@ -14,14 +14,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 
 /**
  * @author 林圣涛
  */
 @Controller
-@Api(tags = "OrderController",description = "交易市场")
+@Api(tags = "OrderController",value = "交易市场")
 @RequestMapping("/order")
 public class OrderController {
 
@@ -32,24 +35,22 @@ public class OrderController {
     @ApiOperation("根据id查询已接受订单")
     @RequestMapping(value = "/id",method = RequestMethod.GET)
     @ResponseBody
-    public CommonResult getAcceptedOrders(int id){
+    public CommonResult<Order> getAcceptedOrders(int id){
         Order byId = orderService.getById(id);
         if(byId != null){
             return CommonResult.success(byId);
         }else {
-            return CommonResult.failed();
+            return CommonResult.failed("请仔细检查id或者联系管理员");
         }
     }
 
     @ApiOperation("查询已接受订单")
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     @ResponseBody
-    public CommonResult getOrderList(){
+    public CommonResult<List<Order>> getOrderList(){
         List<Order> list = orderService.list();
-        if(list != null){
-            return CommonResult.success(list);
-        }else {
-            return CommonResult.failed();
-        }
+        List<Order> collect = new ArrayList<>(Optional.ofNullable(list)
+                .orElse(Collections.emptyList()));
+        return collect.isEmpty() ? CommonResult.failed("Failed to retrieve order list") : CommonResult.success(collect);
     }
 }
